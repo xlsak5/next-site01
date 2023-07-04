188b779c460fc4bd90f5e43f9b1527ae9ace75a5
testAnGyonam <skadldldl123@gmail.com>
2023-07-04 16:51:10 +0900

    2023.07.04

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Slider from '@/component/slider/Slider'
+import IntroMain from "@/component/intro/IntroMain"
 import Portfolio from '@/component/portfolio/PortfolioMain'
 import Youtube from '@/component/youtube/Youtube'
 import Movie2 from '@/component/movie/Movie2'
@@ -10,6 +11,7 @@ const Home = () => {
   return (
     <>
       <Slider />
+      <IntroMain />
       <Portfolio />
       <Youtube />
       <Movie2 />
